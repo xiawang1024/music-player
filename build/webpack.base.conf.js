b0d3ea71483d1db570c430a41b9e627b1844067a
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2017-12-20 16:36:24 +0800

    配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -27,6 +27,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'base':resolve('src/base'),
       'components':resolve('src/components'),
       'common':resolve('src/common'),
       'api':resolve('src/api'),
