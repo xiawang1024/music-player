481cf09df772f7308990ad40478c137defcab881
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2017-12-21 18:14:11 +0800

    歌手api

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -6,12 +6,36 @@
 
 
 <script>
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+
 export default {
-  
+  name:'singer',
+  data() {
+    return {
+      singers:[]
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods:{
+    _getSingerList(){
+      getSingerList().then((res) => {
+        if(res.code === ERR_OK){
+          this.singers = res.data.list        
+        }
+      })
+    }
+  }
 }
 </script>
 
 
 <style lang="stylus" scoped>
-
+.singer
+  position fixed
+  top 88px
+  bottom 0
+  width 100%
 </style>
