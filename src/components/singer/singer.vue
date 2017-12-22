966524b2da2e71aaf277e22fb6c4b11e05da6088
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2017-12-22 13:23:26 +0800

    歌手数据聚合

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -8,6 +8,10 @@
 <script>
 import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 
 export default {
   name:'singer',
@@ -23,9 +27,40 @@ export default {
     _getSingerList(){
       getSingerList().then((res) => {
         if(res.code === ERR_OK){
-          this.singers = res.data.list        
+          this.singers = res.data.list    
+          console.log('------------------------------------');
+          console.log(this._normalizeSinger(this.singers));
+          console.log('------------------------------------');    
         }
       })
+    },
+    _normalizeSinger(list){
+      let map = {
+        hot:{
+          title:HOT_NAME,
+          items:[]
+        }
+      }
+      list.forEach((item,index) => {
+        if(index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id:item.Fsinger_mid,
+            name:item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if(!map[key]){
+          map[key] = {
+            title:key,
+            items:[]
+          }
+        }
+        map[key].items.push(new Singer({
+          id:item.Fsinger_mid,
+          name:item.Fsinger_name
+        }))
+      })
+      return map
     }
   }
 }
