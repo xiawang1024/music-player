7abfdb41d78552f7fa6423bb1288691e710874d0
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2017-12-22 14:34:55 +0800

    歌手排序

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -60,7 +60,21 @@ export default {
           name:item.Fsinger_name
         }))
       })
-      return map
+      //为了得到有序列表，我们需要处理map
+      let hot = []
+      let ret = []
+      for(let key in map) {
+        let val = map[key]
+        if(val.title.match(/[a-zA-Z]/)){
+          ret.push(val)
+        }else if(val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
     }
   }
 }
